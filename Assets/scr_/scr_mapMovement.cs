2f14913676edfc0c248d7f4d0ee800e7a6321c84
tobias02121999 <hedgefoggames@outlook.com>
2018-04-23 15:43:20 +0200

    V1.0

---
+++ Assets/scr_/scr_mapMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_mapMovement : MonoBehaviour {
+
+    private float rotationX;
+    private float rotationZ;
+
+    public float sensitivity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotationX += Input.GetAxis("Vertical") * sensitivity;
+        rotationZ -= Input.GetAxis("Horizontal") * sensitivity;
+
+        transform.rotation = Quaternion.Euler(rotationX, 0f, rotationZ);
+
+        Debug.Log(Input.GetButton("AButton"));
+	}
+}

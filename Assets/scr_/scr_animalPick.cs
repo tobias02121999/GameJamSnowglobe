3d385aec6fd0f0a8f3b133c1dd7e3ca21a40ebdf
Daan de Bruijn <ddebruijn9@gmail.com>
2018-04-23 16:18:16 +0200

    V3.0

---
+++ Assets/scr_/scr_animalPick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animalPickup : MonoBehaviour {
+	
+	public GameObject car;
+	public GameObject varken;
+	public GameObject kip;
+	public GameObject schaap;
+
+	void onTriggerEnter(Collision collision){
+		if (collision.gameObject.tag == "Varken"){
+			varken.SetActive(true);
+			Destroy(collision.gameObject);
+		} else if (collision.gameObject.tag == "Kip"){
+			kip.SetActive(true);
+			Destroy(collision.gameObject);
+		} else if (collision.gameObject.tag == "Schaap"){
+			schaap.SetActive(true);
+			Destroy(collision.gameObject);
+		}
+	}
+}
+
+

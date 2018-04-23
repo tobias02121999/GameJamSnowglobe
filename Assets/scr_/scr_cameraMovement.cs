b8e545995ff1a978763c610cd8391fa551650b13
tobias02121999 <hedgefoggames@outlook.com>
2018-04-23 16:35:16 +0200

    V4.0

---
+++ Assets/scr_/scr_cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_cameraMovement : MonoBehaviour {
+
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x + xOffset, player.position.y + yOffset, player.position.z + zOffset);
+	}
+}
